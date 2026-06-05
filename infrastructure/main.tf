82d35fc02b6886d32b6ca366d0e2d7b403fe7e6d
Raksbhat <rakshitb44@gmail.com>
2026-06-05 13:30:22 +0000

    Add S3 backend for Terraform state

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "cloudsentinel-tfstate-923187443111"
+    key    = "cloudsentinel/terraform.tfstate"
+    region = "eu-north-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -19,7 +25,9 @@ resource "aws_iam_role" "lambda_role" {
     Statement = [{
       Action    = "sts:AssumeRole"
       Effect    = "Allow"
-      Principal = { Service = "lambda.amazonaws.com" }
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
     }]
   })
 }
@@ -57,65 +65,66 @@ resource "aws_iam_role_policy" "lambda_policy" {
           "secretsmanager:GetSecretValue"
         ]
         Resource = "arn:aws:secretsmanager:eu-north-1:923187443111:secret:cloudsentinel/*"
-      }
-    ]
-  })
-}
+       }
+     ]
+   })
+ }
 
-resource "aws_lambda_function" "cloudsentinel_api" {
-  filename         = "lambda.zip"
-  function_name    = "cloudsentinel-api"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "handler.handler"
-  runtime          = "python3.11"
-  timeout          = 30
-  memory_size      = 256
+ resource "aws_lambda_function" "cloudsentinel_api" {
+   filename      = "lambda.zip"
+   function_name = "cloudsentinel-api"
+   role          = aws_iam_role.lambda_role.arn
+   handler       = "handler.handler"
+   runtime       = "python3.11"
+   timeout       = 30
+   memory_size   = 256
 
-  environment {
-    variables = {
-      ENV = "production"
-    }
-  }
-}
+   environment {
+     variables = {
+       ENV = "production"
+     }
+   }
+ }
 
-resource "aws_apigatewayv2_api" "cloudsentinel" {
-  name          = "cloudsentinel-api"
-  protocol_type = "HTTP"
+ resource "aws_apigatewayv2_api" "cloudsentinel" {
+   name          = "cloudsentinel-api"
+   protocol_type = "HTTP"
 
-  cors_configuration {
-    allow_origins = ["*"]
-    allow_methods = ["GET", "POST", "OPTIONS"]
-    allow_headers = ["*"]
-  }
-}
+   cors_configuration {
+     allow_origins = ["*"]
+     allow_methods = ["GET", "POST", "OPTIONS"]
+     allow_headers = ["*"]
+   }
+ }
 
-resource "aws_apigatewayv2_integration" "lambda" {
-  api_id             = aws_apigatewayv2_api.cloudsentinel.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.cloudsentinel_api.invoke_arn
-  payload_format_version = "2.0"
-}
+ resource "aws_apigatewayv2_integration" "lambda" {
+   api_id                 = aws_apigatewayv2_api.cloudsentinel.id
+   integration_type       = "AWS_PROXY"
+   integration_uri        = aws_lambda_function.cloudsentinel_api.invoke_arn
+   payload_format_version = "2.0"
+ }
 
-resource "aws_apigatewayv2_route" "default" {
-  api_id    = aws_apigatewayv2_api.cloudsentinel.id
-  route_key = "$default"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
-}
+ resource "aws_apigatewayv2_route" "default" {
+   api_id    = aws_apigatewayv2_api.cloudsentinel.id
+   route_key = "$default"
+   target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+ }
 
-resource "aws_apigatewayv2_stage" "prod" {
-  api_id      = aws_apigatewayv2_api.cloudsentinel.id
-  name        = "$default"
-  auto_deploy = true
-}
+ resource "aws_apigatewayv2_stage" "prod" {
+   api_id      = aws_apigatewayv2_api.cloudsentinel.id
+   name        = "$default"
+   auto_deploy = true
+ }
 
-resource "aws_lambda_permission" "api_gateway" {
-  statement_id  = "AllowAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.cloudsentinel_api.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.cloudsentinel.execution_arn}/*/*"
-}
+ resource "aws_lambda_permission" "api_gateway" {
+   statement_id  = "AllowAPIGateway"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.cloudsentinel_api.function_name
+   principal     = "apigateway.amazonaws.com"
+   source_arn    = "${aws_apigatewayv2_api.cloudsentinel.execution_arn}/*/*"
+ }
 
-output "api_url" {
-  value = aws_apigatewayv2_stage.prod.invoke_url
-}
+ output "api_url" {
+   value = aws_apigatewayv2_stage.prod.invoke_url
+ }
+
