212b8c75c9970a5aace4c08fc410eb0f03b12557
Rakshit Bhat <95375478+Raksbhat@users.noreply.github.com>
2026-06-09 20:35:34 +0530

    Remove Lambda and API Gateway resources
    Removed AWS Lambda and API Gateway resources from the Terraform configuration.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,4 @@
+
 terraform {
   backend "s3" {
     bucket = "cloudsentinel-tfstate-923187443111"
@@ -161,85 +162,5 @@ resource "aws_lambda_permission" "api_gateway" {
 output "api_url" {
   value = aws_apigatewayv2_stage.prod.invoke_url
 }
-
-
-        Effect = "Allow"
-        Action = [
-          "secretsmanager:GetSecretValue"
-        ]
-        Resource = "arn:aws:secretsmanager:eu-north-1:923187443111:secret:cloudsentinel/*"
-       }
-     ]
-   })
- }
-
-
- resource "aws_lambda_function" "cloudsentinel_api" {
-   filename      = "lambda.zip"
-   function_name = "cloudsentinel-api"
-   role          = aws_iam_role.lambda_role.arn
-   handler       = "handler.handler"
-   runtime       = "python3.11"
-   timeout       = 30
-   memory_size   = 256
-
-
-   environment {
-  variables = {
-    ENV            = "production"
-    FINDINGS_TABLE = aws_dynamodb_table.findings.name
-  }
-}
- }
-
-
- resource "aws_apigatewayv2_api" "cloudsentinel" {
-   name          = "cloudsentinel-api"
-   protocol_type = "HTTP"
-
-
-   cors_configuration {
-     allow_origins = ["*"]
-     allow_methods = ["GET", "POST", "OPTIONS"]
-     allow_headers = ["*"]
-   }
- }
-
-
- resource "aws_apigatewayv2_integration" "lambda" {
-   api_id                 = aws_apigatewayv2_api.cloudsentinel.id
-   integration_type       = "AWS_PROXY"
-   integration_uri        = aws_lambda_function.cloudsentinel_api.invoke_arn
-   payload_format_version = "2.0"
- }
-
-
- resource "aws_apigatewayv2_route" "default" {
-   api_id    = aws_apigatewayv2_api.cloudsentinel.id
-   route_key = "$default"
-   target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
- }
-
-
- resource "aws_apigatewayv2_stage" "prod" {
-   api_id      = aws_apigatewayv2_api.cloudsentinel.id
-   name        = "$default"
-   auto_deploy = true
- }
-
-
- resource "aws_lambda_permission" "api_gateway" {
-   statement_id  = "AllowAPIGateway"
-   action        = "lambda:InvokeFunction"
-   function_name = aws_lambda_function.cloudsentinel_api.function_name
-   principal     = "apigateway.amazonaws.com"
-   source_arn    = "${aws_apigatewayv2_api.cloudsentinel.execution_arn}/*/*"
- }
-
-
- output "api_url" {
-   value = aws_apigatewayv2_stage.prod.invoke_url
- }
-
 
 
