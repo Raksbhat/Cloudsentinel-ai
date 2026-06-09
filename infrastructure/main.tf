00287b2f9d648fac91c11183da967d5a02b92a99
Rakshit Bhat <95375478+Raksbhat@users.noreply.github.com>
2026-06-09 19:35:59 +0530

    Add DynamoDB table and update Lambda function config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,9 +1,11 @@
+
 terraform {
   backend "s3" {
     bucket = "cloudsentinel-tfstate-923187443111"
     key    = "cloudsentinel/terraform.tfstate"
     region = "eu-north-1"
   }
+
 
   required_providers {
     aws = {
@@ -13,12 +15,37 @@ terraform {
   }
 }
 
+
 provider "aws" {
   region = "eu-north-1"
 }
+resource "aws_dynamodb_table" "findings" {
+  name         = "cloudsentinel-findings"
+  billing_mode = "PAY_PER_REQUEST"
 
+
+  hash_key = "finding_id"
+
+
+  attribute {
+    name = "finding_id"
+    type = "S"
+  }
+
+
+  ttl {
+    attribute_name = "expires_at"
+    enabled        = true
+  }
+
+
+  tags = {
+    Project = "CloudSentinel"
+  }
+}
 resource "aws_iam_role" "lambda_role" {
   name = "cloudsentinel-lambda-role"
+
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -32,9 +59,11 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
+
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "cloudsentinel-lambda-policy"
   role = aws_iam_role.lambda_role.id
+
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -48,6 +77,16 @@ resource "aws_iam_role_policy" "lambda_policy" {
         ]
         Resource = "arn:aws:logs:*:*:*"
       },
+      {
+  Effect = "Allow"
+  Action = [
+    "dynamodb:PutItem",
+    "dynamodb:GetItem",
+    "dynamodb:Scan",
+    "dynamodb:UpdateItem"
+  ]
+  Resource = aws_dynamodb_table.findings.arn
+},
       {
         Effect = "Allow"
         Action = [
@@ -70,6 +109,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
    })
  }
 
+
  resource "aws_lambda_function" "cloudsentinel_api" {
    filename      = "lambda.zip"
    function_name = "cloudsentinel-api"
@@ -79,16 +119,20 @@ resource "aws_iam_role_policy" "lambda_policy" {
    timeout       = 30
    memory_size   = 256
 
+
    environment {
-     variables = {
-       ENV = "production"
-     }
-   }
+  variables = {
+    ENV            = "production"
+    FINDINGS_TABLE = aws_dynamodb_table.findings.name
+  }
+}
  }
+
 
  resource "aws_apigatewayv2_api" "cloudsentinel" {
    name          = "cloudsentinel-api"
    protocol_type = "HTTP"
+
 
    cors_configuration {
      allow_origins = ["*"]
@@ -97,6 +141,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
    }
  }
 
+
  resource "aws_apigatewayv2_integration" "lambda" {
    api_id                 = aws_apigatewayv2_api.cloudsentinel.id
    integration_type       = "AWS_PROXY"
@@ -104,17 +149,20 @@ resource "aws_iam_role_policy" "lambda_policy" {
    payload_format_version = "2.0"
  }
 
+
  resource "aws_apigatewayv2_route" "default" {
    api_id    = aws_apigatewayv2_api.cloudsentinel.id
    route_key = "$default"
    target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
  }
 
+
  resource "aws_apigatewayv2_stage" "prod" {
    api_id      = aws_apigatewayv2_api.cloudsentinel.id
    name        = "$default"
    auto_deploy = true
  }
+
 
  resource "aws_lambda_permission" "api_gateway" {
    statement_id  = "AllowAPIGateway"
@@ -124,7 +172,10 @@ resource "aws_iam_role_policy" "lambda_policy" {
    source_arn    = "${aws_apigatewayv2_api.cloudsentinel.execution_arn}/*/*"
  }
 
+
  output "api_url" {
    value = aws_apigatewayv2_stage.prod.invoke_url
  }
+
+
 
